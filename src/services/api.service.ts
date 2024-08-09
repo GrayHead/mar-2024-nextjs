a2e6e1d65714c5e6f492cfd0ae136ec097a6a447
GrayHead <ss.zhuravlov@gmail.com>
2024-08-09 21:26:50 +0300

    next js lesson 2 server actions db init cache

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,8 +1,8 @@
 export const getAllUsers = async (): Promise<any[]> => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/users',{
+    let response = await fetch('https://jsonplaceholder.typicode.com/users', {
         // cache: 'no-store',
-        next:{
-            revalidate: 60*60*24
+        next: {
+            revalidate: 60 * 60 * 24
         }
     })
         .then(value => value.json());
@@ -14,6 +14,4 @@ export const getPostsOfUser = async (id: number) => {
     let postsOfUser = await fetch('https://jsonplaceholder.typicode.com/users/' + id + '/posts')
         .then(value => value.json());
     return postsOfUser;
-
-
 }
